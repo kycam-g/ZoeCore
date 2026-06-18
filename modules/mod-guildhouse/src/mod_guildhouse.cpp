b476f2c6846d66d768a1bc3b5ceb6d732a9ad48b
mkcoder-g <mgalhardo.coder@gmail.com>
2026-06-18 14:57:44 -0300

    mod-guildhouse update fix compile

--- modules/mod-guildhouse/src/mod_guildhouse.cpp
+++ modules/mod-guildhouse/src/mod_guildhouse.cpp
@@ -310,7 +310,7 @@ public:
                 uint32 lowguid = fields[0].Get<int32>();
                 if (CreatureData const* cr_data = sObjectMgr->GetCreatureData(lowguid))
                 {
-                    if (Creature* creature = map->GetCreature(ObjectGuid::Create<HighGuid::Unit>(cr_data->id1, lowguid)))
+                    if (Creature* creature = map->GetCreature(ObjectGuid::Create<HighGuid::Unit>(cr_data->id, lowguid)))
                     {
                         creature->CombatStop();
                         creature->DeleteFromDB();
@@ -567,7 +567,7 @@ public:
                 uint32 lowguid = fields[0].Get<uint32>();
                 if (CreatureData const* cr_data = sObjectMgr->GetCreatureData(lowguid))
                 {
-                    if (Creature* creature = map->GetCreature(ObjectGuid::Create<HighGuid::Unit>(cr_data->id1, lowguid)))
+                    if (Creature* creature = map->GetCreature(ObjectGuid::Create<HighGuid::Unit>(cr_data->id, lowguid)))
                     {
                         creature->CombatStop();
                         creature->DeleteFromDB();
